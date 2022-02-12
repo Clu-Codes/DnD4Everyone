671fa71b7284fb31f262ca902a923955a195df55
Connor Cluster <Connor@MacBook-Pro.localdomain>
2022-02-11 23:26:44 -0500

    add initial user model, connections and routes.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,17 @@
 const { Sequelize } = require('sequelize');
 
+require('dotenv').config();
 
-const Sequelize = new Sequelize('database', 'username', 'password', {
-    
-});
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+}
+else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: 'localhost', 
+        dialect: 'mysql',
+        port: 3030
+    });
 
+}
+
+module.exports = sequelize;
